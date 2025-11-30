591a1214f4a0521defb3d960e22db4dba91d44dd
MIROSKA <quispechilojorge@gmail.com>
2025-11-30 10:41:20 -0500

    project setup

---
+++ app/menu/page.tsx
@@ -0,0 +1,10 @@
+import { MenuItem, MenuCategory } from "@/components";
+
+export default function Menu() {
+  return (
+    <div>
+      <h2>Categoria</h2>
+      <MenuCategory />
+    </div>
+  );
+}
---
+++ components/MenuCategory.tsx
@@ -0,0 +1,11 @@
+import { MenuItem } from "./MenuItem";
+
+export const MenuCategory = () => {
+  return (
+    <div>
+        <h2>Categoria 2</h2>
+      <MenuItem></MenuItem>
+
+    </div>
+  );
+};
