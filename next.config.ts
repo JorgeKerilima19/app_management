becf4fca59dcc9b22375355ed4114b72d0479fd4
MIROSKA <quispechilojorge@gmail.com>
2026-01-13 00:16:01 -0500

    ...

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  allowedDevOrigins: [
-    // Add your dev machine's LAN IP(s)
-    "192.168.137.*",
-    // Optionally allow whole subnet (use cautiously)
-  ],
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
